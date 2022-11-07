d1e9714e67a98900d7cb547c9e67fd95607b47fa
Wlkn <alibachar27@gmail.com>
2022-11-06 22:10:50 -0500

    3rd commit

--- script.js
+++ script.js
@@ -3,22 +3,114 @@ const quizPage = document.querySelector(".quizPage");
 const startButton = document.querySelector("#start-button");
 const timeLimit = document.querySelector("#timeLimit");
 const question = document.querySelector("#question");
-const answers = document.querySelector("#answers");
+const answers = document.querySelectorAll(".answer");
 const bubbleAnswers = document.querySelector("#bubbleAnswers");
+const inputBar = document.querySelector("#questions");
+const questionNbStr = document.querySelector("#questions");
+const minQuestion = 0;
+const maxQuestion = 10;
+let apiUrl = "https://the-trivia-api.com/api/questions?";
+let loadedQuestions = 0;
+let currentQuestions = {};
+let questions = [];
+let score = 0;
+let questionNb = parseInt(questionNbStr.value);
+
 startButton.addEventListener("click", () => {
-    frontPage.style.display = "none";
-    quizPage.style.display = "block";
+    questionNb = parseInt(questionNbStr.value);
+    if (
+        questionNbStr.value.includes(".") == false &&
+        questionNb > minQuestion &&
+        questionNb <= maxQuestion
+    ) {
+        console.log("Number of questions has been accepted");
+        frontPage.style.display = "none";
+        quizPage.style.display = "block";
+        apiUrl = `${apiUrl}limit=${questionNb}`;
+
+        fetchJSON(); // get the json file
+
+        console.log("This should print out the Custom LIMIT url" + apiUrl);
+
+        createDots(); // this creates the number of dots that will display if you got a good answer or not.
+
+        startTimer();
+    } else {
+        console.log("Number of quesions has been revoked");
+        alert("Please enter a valid number of questions.");
+    }
 });
 
+async function fetchJSON() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    // getNewQuestion(data);
+    // getGoodAnswer(data);
+    displayQuestionAndAnswers(data);
+}
+
+function createDots() {
+    for (let i = 0; i < questionNb; i++) {
+        var newSpan = document.createElement("span");
+        newSpan.classList.add("dot");
+
+        var bubbleAnswersDiv = document.getElementById("bubbleAnswers");
+        bubbleAnswersDiv.appendChild(newSpan);
+    }
+}
+
+answers[1].addEventListener("click", () => {
+    console.log("cledab")
+})
+
+function displayQuestionAndAnswers(data) {
+    //displays a question
+
+    question.innerHTML = data[0].question;
+    answers[0].innerHTML = data[0].correctAnswer;
+    answers[1].innerHTML = data[0].incorrectAnswers[0];
+    answers[2].innerHTML = data[0].incorrectAnswers[1];
+    answers[3].innerHTML = data[0].incorrectAnswers[2]
+    console.log(data[0].correctAnswer);
+}
+
+// function getNewQuestion(data) {
+//     // function showQuestion(data){
+
+//     question.innerHTML = data[0].question;
+// }
+
+// function getGoodAnswer(data) {
+//     let correctAnswer = data.correctAnswer;
+//     let incorrectAnswer = data[].incorrectAnswers;
+//     console.log(incorrectAnswer)
+//     let optionsList = incorrectAnswer;
+//     optionsList.splice(
+//         Math.floor(Math.random() * (incorrectAnswer.length + 1)),
+//         0,
+//         correctAnswer
+//     );
+//     console.log(correctAnswer);
+//     choicesList[0].textContent = data[0].correctAnswer;
+//     console.log(
+//         `This should print out the correct answer [0] ${data[0].correctAnswer}`
+//     );
+//     console.log(
+//         `This should print out the text content of choices [0]${choicesList[0].textContent}`
+//     );
+// }
+
+//make the api work with the start button and nb of questions //DONE
+//import JSON // DONE
+//make answers change the question and answers
+//implement timer logic
+//implement good answers/bad answers displayer (bottom left)
+//make end game screen
 //
-//
-//
-//
-//
-//
-//
-//
-//
+//if you have enough time:
+//make website mobile-compatible
+//minor adjustments
 //
 //
 //
@@ -128,8 +220,6 @@ document.getElementById("progressBar").innerHTML = `
   )}</span>
 </div>
 `;
-
-startTimer();
 
 function onTimesUp() {
     clearInterval(timerInterval);
